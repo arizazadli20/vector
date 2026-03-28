9ca1d0861c92124952719c98e0f5b1c6b519e392
Ariz Azadli <arizazadli20@gmail.com>
2026-03-28 01:22:44 +0100

    Initial Commit

---
+++ Vectormac/VectormacApp.swift
@@ -0,0 +1,17 @@
+//
+//  VectormacApp.swift
+//  Vectormac
+//
+//  Created by ARIZ AZADOV on 28/03/26.
+//
+
+import SwiftUI
+
+@main
+struct VectormacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VectormacTests/VectormacTests.swift
@@ -0,0 +1,17 @@
+//
+//  VectormacTests.swift
+//  VectormacTests
+//
+//  Created by ARIZ AZADOV on 28/03/26.
+//
+
+import Testing
+@testable import Vectormac
+
+struct VectormacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
